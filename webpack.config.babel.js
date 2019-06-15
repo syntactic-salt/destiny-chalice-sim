77f9527618f3ebf844c0c5dab49a40acb7274333
syntactic-salt <kennethpereira@gmail.com>
2019-06-14 23:44:28 -0400

    bundle splitting and cache busting

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,79 +2,102 @@ import path from 'path';
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import MiniCSSExtractPlugin from 'mini-css-extract-plugin';
 
-export default {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve('build'),
-        filename: 'bundle.js',
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: { loader: 'babel-loader' },
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: MiniCSSExtractPlugin.loader,
-                        options: { hmr: true },
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true,
-                            modules: true,
-                            localIdentName: '[name]__[local]___[hash:base64:5]',
-                            camelCase: true,
+export default (env) => {
+    return {
+        entry: {
+            index: './src/index.js'
+        },
+        output: {
+            path: path.resolve(__dirname, 'build'),
+                filename: `[name]${env.production ? '.[contenthash]' : ''}.js`,
+                chunkFilename: `[name]${env.production ? '.[contenthash]' : ''}.js`
+        },
+        resolve: {
+            extensions: ['.js', '.jsx'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    use: { loader: 'babel-loader' },
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {
+                            loader: MiniCSSExtractPlugin.loader,
+                            options: { hmr: true },
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true,
+                                modules: true,
+                                localIdentName: '[name]__[local]___[hash:base64:5]',
+                                camelCase: true,
+                            },
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: { sourceMap: true },
+                        },
+                    ]
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        {
+                            loader: MiniCSSExtractPlugin.loader,
+                            options: { hmr: true },
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: { sourceMap: true }
+                        },
+                    ],
+                },
+                {
+                    test: /\.svg$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: { name: `[name]${env.production ? '.[contenthash]' : ''}.[ext]` },
+                        },
+                    ],
+                },
+            ],
+        },
+        optimization: {
+            runtimeChunk: 'single',
+                splitChunks: {
+                chunks: 'all',
+                    maxInitialRequests: Infinity,
+                    minSize: 0,
+                    cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                            name(module) {
+                            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+                            return `vendor.${packageName.replace('@', '')}`;
                         },
                     },
-                    {
-                        loader: 'sass-loader',
-                        options: { sourceMap: true },
-                    },
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: MiniCSSExtractPlugin.loader,
-                        options: { hmr: true },
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: { sourceMap: true }
-                    },
-                ],
-            },
-            {
-                test: /\.svg$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: { name: '[name].[ext]' },
-                    },
-                ],
-            },
-        ],
-    },
-    plugins: [
-        new HTMLWebpackPlugin(
-            {
-                inject: false,
-                minify: {
-                    collapseWhitespace: true,
-                    removeComments: true,
                 },
-                template: 'src/index.html.ejs',
             },
-        ),
-        new MiniCSSExtractPlugin({ filename: '[name].css' }),
-    ],
+        },
+        plugins: [
+            new HTMLWebpackPlugin(
+                {
+                    inject: false,
+                    minify: env.production ? { collapseWhitespace: true, removeComments: true } : false,
+                    template: 'src/index.html.ejs',
+                },
+            ),
+            new MiniCSSExtractPlugin(
+                {
+                    filename: `[name]${env.production ? '.[contenthash]' : ''}.css`,
+                }
+            ),
+        ],
+    };
 };
